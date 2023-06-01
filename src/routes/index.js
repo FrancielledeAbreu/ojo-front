e62be4e73f0afbad20cfabfb84606215f5d971cd
francielle <francielledeabreus@gmail.com>
2023-06-01 14:38:34 -0300

    feat: routes config

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "../pages/home/home";
+import MovieDetails from "../pages/movieDetails/movieDetails";
+
+//apoio para rotas
+export const routes = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/movie-details", name: "MovieDetails", component: MovieDetails },
+];
+
+const Routes = () => {
+  return (
+    <Switch>
+      {routes.map((route, key) => (
+        <Route key={key} exact path={route.path}>
+          <route.component />
+        </Route>
+      ))}
+    </Switch>
+  );
+};
+
+export default Routes;
